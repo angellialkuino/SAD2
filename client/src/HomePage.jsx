09cee5d121e9e96827115d1d4495c79909299c27
Fatma <fatimahussien2@gmail.com>
2022-08-16 00:05:23 +0800

    Finished homepage. WIP: NavBar routing

--- client/src/HomePage.jsx
+++ client/src/HomePage.jsx
@@ -5,9 +5,26 @@ class HomePage extends Component {
         return (
             <div className="m-5">
                 <h1 className="text-center fw-bold">Custom-made Designs</h1>
-                <div className="vertical-center m-5">
-                    <div className="feed-frame vertical-center">
-                        <img src={process.env.PUBLIC_URL + '/logo.png'} alt="Wedding Invitation" />
+                <div className="vertical-content m-5">
+                    <div className="feed-frame mb-5">
+                        <img src={process.env.PUBLIC_URL + '/images/sample1.jpg'} alt="Debut Invitation" />
+                        <h4 className="text-bg">Debut Invitation</h4>
+                    </div>
+                    <div className="feed-frame mb-5">
+                        <img src={process.env.PUBLIC_URL + '/images/sample2.jpg'} alt="Debut Invitation" />
+                        <h4 className="text-bg">Wedding Invitation</h4>
+                    </div>
+                    <div className="feed-frame mb-5">
+                        <img src={process.env.PUBLIC_URL + '/images/sample3.jpg'} alt="Debut Invitation" />
+                        <h4 className="text-bg">Wedding Invitation</h4>
+                    </div>
+                    <div className="feed-frame mb-5">
+                        <img src={process.env.PUBLIC_URL + '/images/sample4.jpg'} alt="Birthday Invitation" />
+                        <h4 className="text-bg">Birthday Invitation</h4>
+                    </div>
+                    <div className="feed-frame ">
+                        <img src={process.env.PUBLIC_URL + '/images/sample5.jpg'} alt="Debut Invitation" />
+                        <h4 className="text-bg">Wedding Invitation</h4>
                     </div>
                 </div>
             </div>
--- client/src/NavBar.jsx
+++ client/src/NavBar.jsx
@@ -1,4 +1,13 @@
 import React, { Component } from 'react';
+import AboutUs from './AboutUs';
+import App from './App';
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Link
+} from 'react-router-dom';
+
 class NavBar extends Component {
     render() {
         return (
@@ -6,18 +15,25 @@ class NavBar extends Component {
                 <nav className="navbar navbar-expand-lg">
                     <div className="container-fluid m-2 ms-5 me-5">
                         {/* NEED: provide links */}
-                        <a className="navbar-brand" href="#">Crafter's Haven</a>
-                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <Router>
+                            <Link to="/" className="navbar-brand">Crafter's Haven</Link>
+                            <div className="collapse navbar-collapse" id="navbarSupportedContent">
 
-                            <div className="d-grid mx-auto gap-2 d-md-flex">
-                                <a className="nav-link me-md-2 m-2" to="/">About Us</a>
-                                <a className="rounded-pill btn btn-info fw-bold me-md-2" to="/">Order Now</a>
+                                <div className="d-grid mx-auto gap-2 d-md-flex">
+                                    <Link to='/about' className="nav-link me-md-2 m-2">About Us</Link>
+                                    <a className="rounded-pill btn btn-info fw-bold me-md-2" to="/">Order Now</a>
+                                </div>
+                                <div className="d-grid gap-2 d-md-flex justify-content-md-end">
+                                    <a className="rounded-pill btn btn-info fw-bold me-md-2" to="/">Sign Up</a>
+                                    <a to="/login" className="rounded-pill btn btn-info fw-bold me-md-2">Login</a>
+                                </div>
+
+                                {/* <Routes>
+                                    <Route exact path='/about' element={< AboutUs />}></Route>
+                                </Routes> */}
                             </div>
-                            <div className="d-grid gap-2 d-md-flex justify-content-md-end">
-                                <a className="rounded-pill btn btn-info fw-bold me-md-2" to="/">Sign Up</a>
-                                <a to="/login" className="rounded-pill btn btn-info fw-bold me-md-2">Login</a>
-                            </div>
-                        </div>
+                        </Router>
+
                     </div>
                 </nav>
 
