c1c2b1697f484bbd7f8ea55074a4aeaba2928f1a
Leandro Miguel B. Tupas <lmbtupas@addu.edu.ph>
2022-08-18 00:02:06 +0800

    Added terms and conditions

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import AboutUs from "./AboutUs";
 import HomePage from "./HomePage";
 import NavBar from "./NavBar";
+import OrderForm_2 from "./OrderForm_2";
 
 export default class App extends Component {
     render() {
         return <React.Fragment>
             <NavBar />
             <HomePage />
+            <OrderForm_2 />
         </React.Fragment>
     }
 }
