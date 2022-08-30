f689734265f8732d2f15ad8f2ad3e23a8deb3fdb
Leandro Miguel B. Tupas <lmbtupas@addu.edu.ph>
2022-08-30 16:01:28 +0800

    Finalized login page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,6 +35,7 @@ export default class App extends Component {
             <OrderForm_2 /> 
             <StaffAccountUpdateOwner />
             */}
+            
         </React.Fragment>
     }
 }
