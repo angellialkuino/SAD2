5231d376a60a1edc712c37c017300247a340f7e7
Angelli <akmalkuino@addu.edu.ph>
2022-09-08 00:47:49 +0800

    add seeds; modify order CRUD queries

--- server/database/migrations/5_order_purchase_table.js
+++ server/database/migrations/5_order_purchase_table.js
@@ -7,9 +7,9 @@ exports.up = function(knex) {
         table.uuid('OP_id').notNullable().primary();
         table.uuid('user_id').notNullable().references('user_id').inTable('users');
         table.uuid('order_id').notNullable().references('order_id').inTable('order');
-        table.float('unit_cost',2).notNullable();
-        table.float('VAT',2).notNullable();
-        table.float('sub_total',2).notNullable();
+        table.float('unit_cost').notNullable();
+        table.float('VAT').notNullable();
+        table.float('sub_total').notNullable();
         table.dateTime('purchase_date').notNullable();
 
     })
--- server/database/seeds/1_users.js
+++ server/database/seeds/1_users.js
@@ -8,10 +8,9 @@ const bcrypt = require('bcrypt');
 
 exports.seed = async function(knex) {
   // Deletes ALL existing entries
-  pass = await await bcrypt.hash('secret', 10);
   await knex('users').del()
   await knex('users').insert([
-    {user_id: uuid.v4(), role: 'customer', full_name: 'Angelli Kezzed M. Alkuino', phone_number: '09078216011', email: 'aaa@gmail.com', password: pass, fb_account: 'angelli.alkuino', full_address: 'address##adress'},
+    {user_id: uuid.v4(), role: 'customer', full_name: 'Angelli Kezzed M. Alkuino', phone_number: '09078216011', email: 'aaa@gmail.com', password: await bcrypt.hash('uhm', 10), fb_account: 'angelli.alkuino', full_address: 'address##adress'},
     {user_id: uuid.v4(), role: 'customer', full_name: 'Amelie Kris M. Alkuino', phone_number: '09088888', email: 'bbb@gmail.com', password: await bcrypt.hash('shhhh', 10), fb_account: 'aa.alkuino', full_address: 'address##'},
     {user_id: uuid.v4(), role: 'customer', full_name: 'Arnold Kite M. Alkuino', phone_number: '7676767', email: 'ccc@gmail.com', password: await bcrypt.hash('hmmmm', 10), fb_account: 'arnold.alkuino', full_address: 'address##add'},
     {user_id: uuid.v4(), role: 'staff', full_name: 'Andy Kim M. Alkuino', phone_number: '66554', email: 'ddd@gmail.com', password: await bcrypt.hash('idkidkidk', 10), fb_account: 'and.alk', full_address: 'address####'},
