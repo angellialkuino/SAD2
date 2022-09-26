e218fe493230ac0492a42c096936753a583f2ee1
Angelli <akmalkuino@addu.edu.ph>
2022-09-27 00:20:06 +0800

    minor edits to database table; to auth

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -6,6 +6,18 @@ module.exports.isAuth = (req,res,next) => {
 }
 }
 
-module.exports.isAdmin = (req,res,next) => {
-    
+module.exports.isStaff = (req,res,next) => {
+    if (req.user.role == "staff"){
+        next();
+} else {
+    res.status(401).json({msg: 'You are not authorized to view this page'});
+}
+}
+
+module.exports.isOwner = (req,res,next) => {
+    if (req.user.role == "owner"){
+        next();
+} else {
+    res.status(401).json({msg: 'You are not authorized to view this page'});
+}
 }
