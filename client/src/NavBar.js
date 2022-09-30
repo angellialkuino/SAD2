f020ffdd9b4f093f851a2870e7e928c5bd29e88c
Fatma <fatimahussien2@gmail.com>
2022-09-30 08:40:55 +0800

    styling

--- client/src/NavBar.js
+++ client/src/NavBar.js
@@ -9,11 +9,11 @@ function NavBar() {
                 <Link to="/" className="navbar-brand nav-link site-title nav-hover">Crafter's Haven</Link>
                 <ul>
                     <li><Link to='/about-us' className="nav-link nav-text">About Us</Link></li>
-                    <li><Link to='/order-form-login' className="rounded-pill btn btn-info fw-bold nav-hover">Order Now</Link></li>
+                    <li><Link to='/order-form-login' className="rounded-pill btn btn-info nav-hover">Order Now</Link></li>
                 </ul>
                 <ul>
-                    <li><Link to='/sign-up' className="rounded-pill btn btn-info fw-bold nav-hover">Sign Up</Link></li>
-                    <li><Link to='/login' className="rounded-pill btn btn-info fw-bold nav-hover">Login</Link></li>
+                    <li><Link to='/sign-up' className="rounded-pill btn btn-info nav-hover">Sign Up</Link></li>
+                    <li><Link to='/login' className="rounded-pill btn btn-info nav-hover">Login</Link></li>
                 </ul>
             </nav>
 
