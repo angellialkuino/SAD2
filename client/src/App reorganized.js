b81936d7173e1a2ed137ea7b2c0f778091864536
Angelli <akmalkuino@addu.edu.ph>
2022-10-09 02:36:33 +0800

    Added alternate App file; Link in some cust file

---
+++ client/src/App reorganized.js
@@ -0,0 +1,186 @@
+import React, { useState, useRef } from "react";
+import { Route, Routes } from "react-router-dom";
+import ErrorPage from "./ErrorPage";
+import AboutUs from "./AboutUs";
+import HomePage from "./HomePage";
+import TermsAndConditions from "./TermsAndConditions";
+import StaffList from "./StaffList";
+import StaffAccountViewOwner from "./StaffAccountViewOwner";
+import OrderForm1 from "./OrderForm1";
+import CreateStaffAccountOwner from "./CreateStaffAccountOwner";
+import StaffAccountUpdateOwner from "./StaffAccountUpdate";
+import OrderPickup from "./OrderPickup";
+import ShippingAddress from "./ShippingAddress";
+import CustAccDetail from "./CustAccDetail";
+import CustMyOrders from "./CustMyOrders";
+import LoginPage from "./LoginPage";
+import OrderBeingConfirmed from "./OrderBeingConfirmed";
+import OrderForm2 from "./OrderForm2";
+import Downpayment from "./Downpayment";
+import OrderForm5 from "./OrderForm5";
+import InvitationDraftCustomer from './InvitationDraftCustomer'
+import InvitationDraftStaff from './InvitationDraftStaff'
+import OrderDetailsCustomer from "./OrderDetailsCustomer";
+import OrderDetailsStaff from "./OrderDetailsStaff";
+import OrderForm4 from "./OrderForm4";
+import OrderForm3 from "./OrderForm3";
+import OrderHistoryStaff from "./OrderHistoryStaff";
+import OrderListStaff from "./OrderListStaff";
+import OrderDocumentation from "./OrderDocumentation";
+import CheckOrder from "./CheckOrder";
+import OrderDocumentationCustomer from "./OrderDocumentationCustomer";
+import SignUp from "./SignUp";
+import Login from "./Login";
+
+function App() {
+    //if user is logged in and check role(?)
+    const [success, setSuccess] = useState(false);
+    const [roles, setRoles] = useState('');
+    const sumTotal = useRef(0);
+
+    //order form data
+    const [orderData, setOrderData] = useState({
+        inviteType: '',
+        material: '',
+        materialPrice: 0,
+        eventDate: '',
+        motif: '',
+        invitationTitle: '',
+        font: '',
+        contentLink: '',
+        inviteNumbers: '',
+        pegLink: '',
+        pagesPaperAndColor: '',
+        pagesPrice: {
+            pagesSize: '',
+            pagesSizePrice: 0,
+        },
+        envelope: false,
+        envelopePaperAndColor: '',
+        envelopePrice: {
+            envelopeSize: '',
+            envelopeSizePrice: 0,
+        },
+        envelopeLinerPricing: {
+            envelopeLiner: false,
+            envelopeLinerPrice: 0
+        },
+        envelopeLockPricing: {
+            envelopeLock: false,
+            envelopeLockPrice: 0
+        },
+        allTextembossPricing: {
+            allTextEmboss: false,
+            allTextEmbossPrice: 0
+        },
+        headerTextPricing: {
+            headerText: '',
+            headerTextPrice: 0
+        },
+        bodyTextPricing: {
+            bodyText: '',
+            bodyTextPrice: 0
+        },
+        otherPagesPricing: {
+            otherPages: '',
+            otherPagesPrice: 0
+        },
+        coverPricing: {
+            cover: '',
+            coverPrice: 0
+        },
+        cardsPricing: {
+            cards: '',
+            cardPrice: 0
+        },
+        waxSeal: '',
+        sealColor: '',
+        driedFlowers: '',
+        brooch: '',
+        orderFinish: '',
+        receivalMethod: '',
+        priceTotal: '',
+        payentMethod: '',
+    })
+
+
+    return <React.Fragment>
+        <Routes>
+            {/* <Route path='*' element={<ErrorPage />} /> */}
+            {/* Main Page */}
+            {/* <Route path='/' element=
+                {<HomePage
+                    success={success}
+                    roles={roles}
+                />} /> */}
+            <Route path='/about-us' element={<AboutUs />} />
+            <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/login' element=
+                {<Login
+                    success={success}
+                    setSuccess={setSuccess}
+                    roles={roles}
+                    setRoles={setRoles}
+                />} />
+            {/* Customer Order Form */}
+            <Route path='/order-form-login' element={<LoginPage />} />
+            <Route path='/terms-and-conditions' element={<TermsAndConditions />} />
+            <Route path='/order-form-1' element=
+                {<OrderForm1
+                    orderData={orderData}
+                    setOrderData={setOrderData}
+                    sumTotal={sumTotal}
+                />} />
+            <Route path='/order-form-2' element=
+                {<OrderForm2
+                    orderData={orderData}
+                    setOrderData={setOrderData}
+                    sumTotal={sumTotal}
+                />} />
+            <Route path='/order-form-3' element=
+                {<OrderForm3
+                    orderData={orderData}
+                    setOrderData={setOrderData}
+                    sumTotal={sumTotal}
+                />} />
+            <Route path='/order-form-4' element={<OrderForm4 />} />
+            <Route path='/order-form-5' element={<OrderForm5 />} />
+            <Route path='/check-order' element={<CheckOrder />} />
+            <Route path='/order-pickup' element={<OrderPickup />} />
+            <Route path='/shipping-address' element={<ShippingAddress />} />
+            <Route path='downpayment' element={<Downpayment />} />
+            <Route path='/order-payment' element={<OrderBeingConfirmed />} />
+            {/* Customer Account and Order Pages */}
+            <Route path='/account-details' element={<CustAccDetail />} />
+            <Route path='/current-orders' element={<CustMyOrders />} />
+            <Route path='/order-details' element={<OrderDetailsCustomer />} />
+            <Route path='/documentation-of-changes' element={<OrderDocumentationCustomer />} />
+            <Route path='/invitation-draft' element={<InvitationDraftCustomer />} />
+            {/* Staff Pages */}
+            <Route path='/order-list-staff' element={<OrderListStaff />} />
+            <Route path='/order-details-staff' element={<OrderDetailsStaff />} />
+            <Route path='/invitation-draft-staff' element={<InvitationDraftStaff />} />
+            <Route path='/order-history-staff' element={<OrderHistoryStaff />} />
+            {/* Documentaion of Changes for the staff? */}
+            {/* Owner Pages */}
+            <Route path='/create-staff' element={<CreateStaffAccountOwner />} />
+            <Route path='/view-staff' element={<StaffAccountViewOwner />} />
+            <Route path='/update-staff' element={<StaffAccountUpdateOwner />} />
+            <Route path='/staff-list' element={<StaffList />} />
+        </Routes>
+        {/* <OrderForm2
+            orderData={orderData}
+            setOrderData={setOrderData}
+            sumTotal={sumTotal}
+        /> */}
+        {/* <CustMyOrders /> */}
+        <Login
+                    success={success}
+                    setSuccess={setSuccess}
+                    roles={roles}
+                    setRoles={setRoles}
+                />
+    </React.Fragment>
+}
+
+export default App
