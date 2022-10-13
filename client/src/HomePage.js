13a7d00eef72ba306bfabdb4672f77ffce9f4809
Fatma <fatimahussien2@gmail.com>
2022-10-14 02:33:04 +0800

    Updated homepage styling

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -5,32 +5,32 @@ import NavBar from "./NavBar";
 function HomePage() {
     return (
         <>
-                <section>
-                    <h3 className="text-center fw-bold m-5">Custom-made Designs</h3>
-                    <div className="horizontal-content m-5">
-                        <div className="feed-frame mb-5">
-                            <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample1.jpg'} alt="Debut Invitation" />
-                            <h5 className="">Debut Invitation</h5>
-                        </div>
-                        <div className="feed-frame mb-5">
-                            <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample2.jpg'} alt="Debut Invitation" />
-                            <h5 className="">Wedding Invitation</h5>
-                        </div>
-                        <div className="feed-frame mb-5">
-                            <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample3.jpg'} alt="Debut Invitation" />
-                            <h5 className="">Wedding Invitation</h5>
-                        </div>
-                        <div className="feed-frame mb-5">
-                            <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample4.jpg'} alt="Birthday Invitation" />
-                            <h5 className="">Birthday Invitation</h5>
-                        </div>
-                        <div className="feed-frame mb-5">
-                            <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample5.jpg'} alt="Debut Invitation" />
-                            <h5 className="">Wedding Invitation</h5>
-                        </div>
+            <section>
+                <h3 className="text-center fw-bold m-5">Custom-made Designs</h3>
+                <div className="homepage-content m-5">
+                    <div className="feed-frame mb-5">
+                        <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample1.jpg'} alt="Debut Invitation" />
+                        <h5 className="">Debut Invitation</h5>
                     </div>
-                </section>
-            
+                    <div className="feed-frame mb-5">
+                        <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample2.jpg'} alt="Debut Invitation" />
+                        <h5 className="">Wedding Invitation</h5>
+                    </div>
+                    <div className="feed-frame mb-5">
+                        <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample3.jpg'} alt="Debut Invitation" />
+                        <h5 className="">Wedding Invitation</h5>
+                    </div>
+                    <div className="feed-frame mb-5">
+                        <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample4.jpg'} alt="Birthday Invitation" />
+                        <h5 className="">Birthday Invitation</h5>
+                    </div>
+                    <div className="feed-frame mb-5">
+                        <img className="homepage-img" src={process.env.PUBLIC_URL + '/images/sample5.jpg'} alt="Debut Invitation" />
+                        <h5 className="">Wedding Invitation</h5>
+                    </div>
+                </div>
+            </section>
+
         </>
     );
 }
