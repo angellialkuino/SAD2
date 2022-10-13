0413a802c9f3389fbadaef60cf04f69f55690e4b
Leandro Miguel B. Tupas <lmbtupas@addu.edu.ph>
2022-10-13 15:26:12 +0800

    Fixed Order Doc CSS

--- client/src/App.js
+++ client/src/App.js
@@ -152,7 +152,7 @@ function App() {
             <Route path='/staff-list' element={<StaffList />} />
         </Routes>
 
-        <CustAccDetail />
+        <OrderDocumentation />
 
     </React.Fragment>
 }
